ec998642674e4aa04d77d2579d8477ef4e30c210
Wisnu <wisnu21cool@gmail.com>
2023-09-11 22:16:17 +0700

    scroll: update smooth scroll anchor

--- src/js/lib/smooth-scroll.js
+++ src/js/lib/smooth-scroll.js
@@ -1,40 +1,25 @@
 (function () {
   
-  document.querySelectorAll('a[href*="#"]:not([href="#"]):not([href="#0"])').forEach(link => {
-    link.addEventListener('click', event => {
-      event.preventDefault();
+  const anchorLinks = document.querySelectorAll('a[href*="#"]:not([href="#"]):not([href="#0"])');
+  
+  anchorLinks.forEach((link) => {
+    link.addEventListener('click', (e) => {
+      e.preventDefault();
   
       const targetId = link.getAttribute('href').substring(1);
-      const targetElement = document.getElementById(targetId);
   
-      if (targetElement) {
-        event.preventDefault();
-        const duration = 1000; 
+      const targetSection = document.getElementById(targetId);
   
-        const targetOffset = targetElement.getBoundingClientRect().top + window.scrollY;
+      if (targetSection) {
+        const offsetTop = targetSection.getBoundingClientRect().top + window.scrollY;
   
-        const startTime = performance.now();
-        function scrollAnimation(currentTime) {
-          const elapsedTime = currentTime - startTime;
-          const progress = Math.min(elapsedTime / duration, 1);
-          window.scrollTo(0, window.scrollY + (targetOffset - window.scrollY) * progress);
-          if (progress < 1) {
-            requestAnimationFrame(scrollAnimation);
-          }
-        }
-        requestAnimationFrame(scrollAnimation);
-  
-        const handleAnimationEnd = () => {
-          targetElement.focus();
-          if (document.activeElement !== targetElement) {
-            targetElement.setAttribute('tabindex', '-1');
-            targetElement.focus();
-          }
-        };
-  
-        setTimeout(handleAnimationEnd, duration);
+        window.scrollTo({
+          top: offsetTop,
+          behavior: 'smooth',
+        });
       }
     });
   });
+  
   
 })();
